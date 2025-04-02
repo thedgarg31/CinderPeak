3c09653ea2b4e4f4d3cda200500e4715b66e715c
Anirudh Lakhanpal <anirudhlakhanpal123@gmail.com>
2025-04-02 15:43:48 +0530

    Added interfacing functions in PeakStore

--- src/PeakStore.hpp
+++ src/PeakStore.hpp
@@ -5,35 +5,42 @@
 #include <memory>
 #include <type_traits>
 #include <vector>
-namespace CinderPeak
-{
-  namespace PeakStore
-  {
-    const CinderPeak::GraphCreationOptions DEFAULT_GRAPH_OPTIONS(
-        {CinderPeak::GraphCreationOptions::Directed,
-         CinderPeak::GraphCreationOptions::SelfLoops});
-    template <typename VertexType, typename EdgeType>
-    class PeakStore
-    {
-    public:
-      std::unique_ptr<HybridCSR_COO<VertexType, EdgeType>> hybrid_storage = nullptr;
-      std::shared_ptr<GraphInternalMetadata> graph_metadata = nullptr;
-      std::shared_ptr<GraphCreationOptions> create_options = nullptr;
-      std::unique_ptr<AdjacencyList<VertexType, EdgeType>> adjacency_storage = nullptr;
+namespace CinderPeak {
+namespace PeakStore {
+const CinderPeak::GraphCreationOptions
+    DEFAULT_GRAPH_OPTIONS({CinderPeak::GraphCreationOptions::Directed,
+                           CinderPeak::GraphCreationOptions::SelfLoops});
+template <typename VertexType, typename EdgeType> class PeakStore {
+public:
+  std::unique_ptr<HybridCSR_COO<VertexType, EdgeType>> hybrid_storage = nullptr;
+  std::shared_ptr<GraphInternalMetadata> graph_metadata = nullptr;
+  std::shared_ptr<GraphCreationOptions> create_options = nullptr;
+  std::unique_ptr<AdjacencyList<VertexType, EdgeType>> adjacency_storage =
+      nullptr;
 
-      PeakStore(const GraphInternalMetadata &metadata, const GraphCreationOptions &options = DEFAULT_GRAPH_OPTIONS)
-      {
-        graph_metadata = std::make_shared<GraphInternalMetadata>(metadata);
-        create_options = std::make_shared<GraphCreationOptions>(options);
-        hybrid_storage =
-            std::make_unique<HybridCSR_COO<VertexType, EdgeType>>(graph_metadata, create_options);
-        adjacency_storage =
-            std::make_unique<AdjacencyList<VertexType, EdgeType>>(graph_metadata, create_options);
-      }
-      void buildStructures(){
-        
-      }
-    };
+  PeakStore(const GraphInternalMetadata &metadata,
+            const GraphCreationOptions &options = DEFAULT_GRAPH_OPTIONS) {
+    graph_metadata = std::make_shared<GraphInternalMetadata>(metadata);
+    create_options = std::make_shared<GraphCreationOptions>(options);
+    hybrid_storage = std::make_unique<HybridCSR_COO<VertexType, EdgeType>>(
+        graph_metadata, create_options);
+    adjacency_storage = std::make_unique<AdjacencyList<VertexType, EdgeType>>(
+        graph_metadata, create_options);
+  }
+  void addEdge(const VertexType &src, const VertexType &dest,
+               const EdgeType &weight) {
+    adjacency_storage->impl_addEdge(src, dest, weight);
+  }
+  void addEdge(const VertexType &src, const VertexType &dest) {
+    adjacency_storage->impl_addEdge(src, dest);
+  }
+  EdgeType getEdge(const VertexType &src, const VertexType &dest) {
+    return adjacency_storage->impl_getEdge(src, dest);
+  }
+  void addVertex(const VertexType &src) {
+    adjacency_storage->impl_addVertex(src);
+  }
+};
 
-  } // namespace PeakStore
+} // namespace PeakStore
 } // namespace CinderPeak
