e6c35210cfcc47a594cf1d80b42ae5f25c30f224
Anirudh Lakhanpal <anirudhlakhanpal123@gmail.com>
2025-04-14 13:57:31 +0530

    Added GraphMatrix basic interface

---
+++ src/GraphMatrix.hpp
@@ -0,0 +1,28 @@
+#include "StorageEngine/Utils.hpp"
+#include <iostream>
+
+namespace CinderPeak {
+namespace PeakStore {
+template <typename VertexType, typename EdgeType> class PeakStore;
+}
+
+template <typename VertexType, typename EdgeType> class GraphMatrix {
+private:
+  std::unique_ptr<CinderPeak::PeakStore::PeakStore<VertexType, EdgeType>>
+      peak_store;
+
+public:
+  GraphMatrix(const GraphCreationOptions &options =
+                  CinderPeak::PeakStore::DEFAULT_GRAPH_OPTIONS) {
+    CinderPeak::PeakStore::GraphInternalMetadata metadata("graph_matrix");
+    peak_store = std::make_unique<
+        CinderPeak::PeakStore::PeakStore<VertexType, EdgeType>>(metadata,
+                                                                options);
+  }
+  void greet() { std::cout << "\nHello from GraphMatrix\n"; }
+  void addVertex(const VertexType &src) {
+    auto resp = peak_store->addVertex(src);
+    std::cout << "vertex addition success\n";
+  }
+};
+} // namespace CinderPeak
